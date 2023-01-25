a26e59a4f8e7274759a3c5001e86147d856ad692
Letam <davidsonletam@gmail.com>
2023-01-25 06:46:20 +0100

    Add hambbuger menu feature

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import navLinks from "data/NavItems";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 import logoImage from "assets/Logo.png";
 function NavBar() {
@@ -10,7 +11,10 @@ function NavBar() {
 				<Link href="/">
 					<Image src={logoImage} alt="Logo" />
 				</Link>
-				<div className="flex gap-x-5">
+				<button className="text-3xl text-pri sm:hidden">
+					<RxHamburgerMenu />
+				</button>
+				<div className="flex gap-x-5 hidden sm:flex">
 					{navLinks.map((navItem, index) => {
 						return (
 							<Link key={index} href={navItem.href} className="">
