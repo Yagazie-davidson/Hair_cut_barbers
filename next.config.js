92891f17f8349d4c8c8f32601d820f37a9140eb6
Letam <davidsonletam@gmail.com>
2023-01-25 22:43:46 +0100

    Add team page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: [
+			"avatars.githubusercontent.com",
+			"user-images.githubusercontent.com",
+			"cdn.discordapp.com",
+			"i.ibb.co",
+			"i.postimg.cc",
+			"images2.imgbox.com",
+			"raw.githubusercontent.com",
+			"img.youtube.com",
+		],
+		formats: ["image/webp"],
+		unoptimized: process.env.NODE_ENV === "development",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
